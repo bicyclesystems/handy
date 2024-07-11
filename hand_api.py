1943c68909723540ed4c28d0d92b20824172dae1
Stanislava Shestakova <stanislavasalishcheva@yandex.ru>
2024-07-11 21:17:36 +0300

    Rollback

--- hand_api.py
+++ hand_api.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+from coordinate_system import CoordinateSystem
 
 class HandAPI:
     def __init__(self, surface_api):
@@ -11,14 +12,16 @@ class HandAPI:
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5
         )
+        self.coordinate_system = CoordinateSystem()
+        self.prev_finger_tips = None
         self.surface_api = surface_api
 
     def detect_hand(self, image):
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(image_rgb)
+        results = self.hands.process(image)
         
         if results.multi_hand_landmarks:
-            return results.multi_hand_landmarks[0]
+            hand_landmarks = results.multi_hand_landmarks[0]
+            return hand_landmarks
         else:
             return None
 
@@ -46,7 +49,20 @@ class HandAPI:
             self.mp_hands.HandLandmark.PINKY_MCP
         ]
         
-        hand_info['finger_tips'] = [(int(hand_landmarks.landmark[tip].x * w), int(hand_landmarks.landmark[tip].y * h)) for tip in finger_tips]
+        current_finger_tips = [(int(hand_landmarks.landmark[tip].x * w), int(hand_landmarks.landmark[tip].y * h)) for tip in finger_tips]
+        
+        if self.prev_finger_tips is None:
+            self.prev_finger_tips = current_finger_tips
+        else:
+            alpha = 0.5
+            current_finger_tips = [
+                (int(alpha * curr[0] + (1 - alpha) * prev[0]), 
+                 int(alpha * curr[1] + (1 - alpha) * prev[1]))
+                for curr, prev in zip(current_finger_tips, self.prev_finger_tips)
+            ]
+            self.prev_finger_tips = current_finger_tips
+
+        hand_info['finger_tips'] = current_finger_tips
         hand_info['finger_directions'] = [
             (hand_landmarks.landmark[tip].x - hand_landmarks.landmark[base].x, 
              hand_landmarks.landmark[tip].y - hand_landmarks.landmark[base].y)
@@ -71,7 +87,10 @@ class HandAPI:
                 relative_x = index_finger_x - cX
                 relative_y = cY - index_finger_y  
                 
-                hand_info['index_finger_on_surface'] = relative_y <= 50 
+                if relative_y <= 100:
+                    hand_info['index_finger_on_surface'] = True
+                else:
+                    hand_info['index_finger_on_surface'] = False
             else:
                 hand_info['index_finger_on_surface'] = False
         else:
@@ -94,7 +113,24 @@ class HandAPI:
         cv2.putText(image, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, 
                     (0, 255, 0) if hand_info['index_finger_on_surface'] else (0, 0, 255), 2)
 
+        image = self.coordinate_system.draw_hand_axes(image, hand_info)
+
+        return image
+
+    def process_image(self, image):
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        hand_landmarks = self.detect_hand(image_rgb)
+
+        if hand_landmarks:
+            hand_info = self.get_hand_info(image, hand_landmarks)
+            image = self.draw_hand(image, hand_info)
+        else:
+            cv2.putText(image, "No hand detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            self.prev_finger_tips = None
+
+        self.coordinate_system.draw_buttons(image)
+
         return image
 
     def handle_click(self, x, y):
-        pass 
+        self.coordinate_system.handle_click(x, y)
